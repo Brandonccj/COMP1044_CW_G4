5726e8068fb4878172c2f2023e961b919412daa4
Brandonccj <brandonccj@gmail.com>
2026-03-25 10:51:40 +0800

    Added basic Css styling

--- index.php
+++ index.php
@@ -4,38 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Internship Management System - Login</title>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
     <h1>Internship Result Management System</h1>
-    <h2>System Login</h2>
 
-    <form action="actions/login_action.php" method="POST">
-        
-        <div>
-            <label for="role">Login As:</label>
-            <select name="role" id="role" required>
-                <option value="Admin">Admin</option>
-                <option value="Assessor">Assessor</option>
-            </select>
-        </div>
-        <br>
+    <div class="login-container">
+        <h2>System Login</h2>
 
-        <div>
-            <label for="username">Username:</label>
-            <input type="text" name="username" id="username" required>
-        </div>
-        <br>
+        <form action="actions/login_action.php" method="POST">
+            
+            <div class="form-group">
+                <label for="role">Login As:</label>
+                <select name="role" id="role" required>
+                    <option value="Admin">Admin</option>
+                    <option value="Assessor">Assessor</option>
+                </select>
+            </div>
 
-        <div>
-            <label for="password">Password:</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <br>
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" required>
+            </div>
 
-        <button type="submit" name="login_submit">Log In</button>
-        
-    </form>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" name="password" id="password" required>
+            </div>
+
+            <button type="submit" name="login_submit">Log In</button>
+            
+        </form>
+    </div>
 
 </body>
 </html>
